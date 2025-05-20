0d64bf1049a2d7993cd0f09f123b53a090d9ab3d
vjescy <vjescy@pm.me>
2025-05-20 16:27:08 +0200

    added Docker-compose with networking

--- docker_kill.sh
+++ docker_kill.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker kill $(docker ps -q) && docker rm $(docker ps -a -q) && docker volume rm $(docker volume ls -q) && docker rmi $(docker images -q)
+#docker kill $(docker ps -q) && docker rm $(docker ps -a -q) && docker volume rm $(docker volume ls -q) && docker rmi $(docker images -q)
+sudo docker network rm ics-honeypot-hmi_scadalts_1 
+sudo docker-compose down
